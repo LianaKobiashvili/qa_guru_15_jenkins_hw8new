79a168391960103d7bbc4ed5881d35e4566b0148
Liana Kobiashvili <lia297@yandex.ru>
2022-12-02 06:10:20 +0300

    added simple tests

---
+++ src/test/java/tests/simple/DisabledTests.java
@@ -0,0 +1,25 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DisabledTests {
+
+    @Test
+    @Disabled
+
+    void test1() {
+
+        assertTrue(false);
+    }
+
+    @Test
+    @Disabled ("any text")
+    void test2() {
+
+        assertTrue(false);
+    }
+
+}
